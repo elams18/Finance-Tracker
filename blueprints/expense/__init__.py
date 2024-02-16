09155aa0e445a4f1e57109b2dc279d896b9ff8e2
elams18 <elamathis18@gmail.com>
2024-02-16 23:36:01 +0530

    added small UI element + added payment type, payment status in expense model

--- blueprints/expense/__init__.py
+++ blueprints/expense/__init__.py
@@ -7,7 +7,7 @@ from blueprints import Base
 class Expense(Base):
     __tablename__ = 'expense'
 
-    class expense_types(enum.IntEnum):
+    class ExpenseType(enum.IntEnum):
         """Enumeration of
         expense types
         Food, Outing, Groceries, Dresses, Gift, other
@@ -19,11 +19,25 @@ class Expense(Base):
         Gift = 5
         Other = 6
 
+    class ExpensePaymentStatus(enum.StrEnum):
+        PAID = "PA"
+        PENDING = "PN"
+
+    class ExpensePaymentType(enum.StrEnum):
+        URGENT = "URGENT"
+        NORMAL = "NORMAL"
+        IMMEDIATE = "IMMEDIATE"
+
     exp_id = Column(UUID, primary_key=True, default=uuid.uuid4)
     exp_name = Column(String, nullable=False)
     exp_description = Column(String, nullable=True)
     exp_created_at = Column(DateTime, nullable=False)
     exp_updated_at = Column(DateTime, nullable=False)
-    exp_type = Column(Integer, default=expense_types.Food, nullable=False)
+    exp_type = Column(Integer, default=ExpenseType.Food, nullable=False,
+                      choices=ExpenseType.__members__)
+    exp_status = Column(String, default=ExpensePaymentType.NORMAL, nullable=False,
+                        choices=ExpensePaymentStatus.__members__)
+    exp_payment_status = Column(String, default=ExpensePaymentStatus.PENDING, nullable=False,
+                                choices=ExpensePaymentStatus.__members__)
 
     expense_user = Column(UUID, ForeignKey('user_account.id'), nullable=False)
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        bree: ['Bree Serif', ...defaultTheme.fontFamily.sans],
       }
     },
   },
